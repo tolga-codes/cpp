c564fca864cb49592b1a5bdad05aaaa31379f656
Tolga Bagcivan <tobalelel@gmx.de>
2026-05-31 01:17:05 +0200

    test1

---
+++ o/o/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  o
+//
+//  Created by Tolga Bagcivan on 31.05.26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
